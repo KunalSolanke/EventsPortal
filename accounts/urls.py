12f13058a0bcd760e888a288bdaab705b64e5abe
VolDemorT-acer <kunalsolanke1000@gmail.com>
2021-03-06 20:51:07 +0530

    Unneccessary file

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,6 @@ from .views import *
 app_name="accounts"
 
 urlpatterns = [ 
-    path('social-auth/', include('social_django.urls', namespace="social")),
     path('signup/complete/',view=UserSignupCompleteView.as_view(),name='register_view'),
     path('team/create/',view=TeamCreateView.as_view(),name='create_team'),
     path('profile/',view=ProfileView.as_view(),name='profile'),
